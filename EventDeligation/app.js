ec9afa3b3c8a7b27ef25822531165cc1e6ce8e73
Harish PJ <harishsmart114@gmail.com>
2025-11-21 15:59:43 +0530

    default behaviour of event propagation

---
+++ EventDeligation/app.js
@@ -0,0 +1,16 @@
+/*  default behaviour is Bubbling target--->parent
+*/
+
+// document.getElementById("grandparent").addEventListener('click', () => {
+//     console.log("grandparent");
+// });
+// document.getElementById("parent").addEventListener('click', () => {
+//     console.log("parent");
+// });
+// document.getElementById("child").addEventListener('click', () => {
+//     console.log("child");
+// });
+// function fun(event) {
+//     console.log(event);
+// }
+
