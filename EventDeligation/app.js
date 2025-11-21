a84f22ccaeedd5eb398f97168e6af12b38174060
Harish PJ <harishsmart114@gmail.com>
2025-11-21 16:04:13 +0530

    Bubbling nature

--- EventDeligation/app.js
+++ EventDeligation/app.js
@@ -14,3 +14,16 @@
 //     console.log(event);
 // }
 
+// This is what it becomes by default ----bubbling
+document.getElementById("grandparent").addEventListener('click', () => {
+    console.log("grandparent");
+}, { capture: false });
+document.getElementById("parent").addEventListener('click', () => {
+    console.log("parent");
+}, { capture: false });
+document.getElementById("child").addEventListener('click', () => {
+    console.log("child");
+}, { capture: false });
+function fun(event) {
+    console.log(event);
+}
